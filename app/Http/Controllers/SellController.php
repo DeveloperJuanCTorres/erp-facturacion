062c94e2ed38ba28595727b6e83c6df7902aeba6
DesarrolladorJuanCTorres <jctorresdelcastillo@gmail.com>
2025-04-26 01:15:18 -0500

    cambios

--- app/Http/Controllers/SellController.php
+++ app/Http/Controllers/SellController.php
@@ -2383,7 +2383,7 @@ class SellController extends Controller
             }      
             
             // $date_now = Carbon::now()->format('d-m-Y');
-            $date_now = $transaction->date->format('d-m-Y');
+            $date_now = $transaction->transaction_date->format('d-m-Y');
             $store = array(
                 "operacion"=> "generar_comprobante",
                 "tipo_de_comprobante"=> $tipo_comprobante,
