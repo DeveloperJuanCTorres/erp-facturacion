5eaaab73aaf99cd078ee10e53764aac037baba76
Juan Carlos Torres del Castillo <jctorresdelcastillo@gmail.com>
2025-10-06 22:07:51 -0500

    admin

--- app/Http/Controllers/SellController.php
+++ app/Http/Controllers/SellController.php
@@ -106,8 +106,8 @@ class SellController extends Controller
                 $sells->whereIn('transactions.location_id', $permitted_locations);
             }
 
-            // Mostrar solo ventas con invoice_no que empiece con BBB o FFF para admin@admin.com
-            if (auth()->user()->email == 'admin@admin.com') {
+            // Mostrar solo ventas con invoice_no que empiece con BBB o FFF para admin@...
+            if (Str::startsWith(auth()->user()->email, 'admin@')) {
                 $sells->where(function ($q) {
                     $q->where('transactions.invoice_no', 'like', 'BBB%')
                     ->orWhere('transactions.invoice_no', 'like', 'FFF%');
